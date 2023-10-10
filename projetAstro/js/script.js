27521b1a5e6c1e3c5253415b69cd5b90d1bcf2c8
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-10-10 11:43:37 +0200

    101023AM1144

--- projetAstro/js/script.js
+++ projetAstro/js/script.js
@@ -36,9 +36,9 @@ function afficherAnnee() {
     let monOption = document.createElement('option');
     monOption.value = '';
     monOption.textContent = 'Choisissez votre année';
-    document.querySelector('#annees').appendChild(monOption);    
+    document.querySelector('#annees').appendChild(monOption);
     const anneeDepart = 2024;
-    const anneeArrivee = 1901;    
+    const anneeArrivee = 1901;
     for (let i = anneeDepart; i > anneeArrivee; i--) {
         monOption = document.createElement('option');
         monOption.value = i;
@@ -61,10 +61,105 @@ function afficherAnnee() {
     return somme;
 }*/
 
-function fonctionValNum
+// DEFINITIONS DE FONCTIONS
 
+function fonctionValNum(maChaine) {
+    let somme = 0;
+    let maChaineMaj = maChaine.toUpperCase();
+    for (let i = 0; i < maChaineMaj.length; i++) {
+        somme = somme + maChaineMaj.charCodeAt(i) - 64;
+    }
+    return somme;
+}
+
+function calculerSigne(mois) {
+    let signeAstro = '';
+    let tabAstro = ["Capricorne", "Verseau", "Poisson", "Belier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"];
+    signeAstro = tabAstro[mois - 1]; // Le signe astro correspond à la case du tableau Astro à l'indice de la valeur du mois - 1
+
+    return signeAstro;
+}
+// Fonction à effet de bord, fonction impure
+function formOk() {
+    let champsOk;   // : BOOLEEN
+
+    let inputNom = document.getElementById('nomUtilisateur');
+    let inputPrenom = document.getElementById('prenomUtilisateur');
+    let inputJour = document.getElementById('jour');
+    let inputMois = document.getElementById('mois');
+    let inputAnnees = document.getElementById('annees');
+    let inputMail = document.getElementById('email');
+
+    if (
+        inputNom.value == "" ||
+        inputPrenom.value == "" ||
+        inputJour.value == "" ||
+        inputMois.value == "" ||
+        inputAnnees.value == "" ||
+        inputMail.value == ""
+        ) {            
+
+        champsOk = false;
+
+    } else {
+
+        champsOk = true;
+
+    }
+
+    return champsOk;
+}
+
+function calculerPseudo(nom, prenom, mois) {
+    let pseudo = '';
+    pseudo = calculerSigne(mois)+(fonctionValNum(nom) + fonctionValNum(prenom));
+    return pseudo;
+}
+
+function remplirPseudo() {
+    if (formOk()) {
+        let inputNom = document.getElementById('nomUtilisateur');
+        let inputPrenom = document.getElementById('prenomUtilisateur');
+        let inputMois = document.getElementById('mois');
+        let pseudo = calculerPseudo(inputNom.value, inputPrenom.value, inputMois.value)
+        let inputPseudo = document.getElementById('pseudoUtilisateur');
+        inputPseudo.value = pseudo;
+   }
+}
+
+// TESTS
+
+console.log(fonctionValNum("Florian"));
+console.log(calculerSigne(2));
+console.log(calculerPseudo('Olivier', 'Cagnetta', 2));
+
+console.log(formOk());
+
+// APPELS
 
 afficherJour();
 afficherMois();
 afficherAnnee();
+
+
+// Attachement d'écouteurs sans classes
+let inputNom = document.getElementById('nomUtilisateur');
+let inputPrenom = document.getElementById('prenomUtilisateur');
+let inputMois = document.getElementById('mois');
+let inputAnnees = document.getElementById('annees');
+let inputJour = document.getElementById('jour');
+let inputMail = document.getElementById('email');
+
+inputNom.addEventListener('input', remplirPseudo);
+inputPrenom.addEventListener('input', remplirPseudo);
+inputMois.addEventListener('change', remplirPseudo);
+inputAnnees.addEventListener('change', remplirPseudo);
+inputJour.addEventListener('change', remplirPseudo);
+inputMail.addEventListener('input', remplirPseudo);
+
+// Attachement d'écouteurs avec classes
+// A FAIRE !
+
+
+
 // console.log(fonctionValNum('baba'));
