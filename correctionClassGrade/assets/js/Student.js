805fc4541f5a0e206d4729fd6ee906648b677f31
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-11-09 11:04:18 +0100

    091123AM1105

--- correctionClassGrade/assets/js/Student.js
+++ correctionClassGrade/assets/js/Student.js
@@ -3,14 +3,36 @@ class Student
     constructor(_studentFromJson) {
         this.firstName = '';
         this.lastName = '';
-        this.grade = _studentFromJson.grade;
+        this.setGrade(_studentFromJson.grade);
         this.setFullName(_studentFromJson.fullname);
+    }
+
+    setGrade(_grade) {
+        // on controle si la note est valide
+        if(_grade < 0 || _grade > 20) {
+            throw new Error('La note doit être comprise entre 0 et 20');
+        }
+        this.grade = _grade;
     }
 
     setFullName(_fullName) {
         let myFullName = _fullName.split(' ');
+        
+        if(myFullName.lenght < 2) {
+            throw new Error('Nom ou prénom invalide(s)');
+        }
+
+        if(myFullName[0].length < 2) {
+            throw new Error('Le nom est invalide');
+        }
+        if(myFullName[1].length < 2) {
+            throw new Error('Le prénom est invalide');
+        }
+
+        
         this.lastName = myFullName[0];
         this.firstName = myFullName[1];
+
     }
 }
 
--- correctionClassGrade/assets/js/main.js
+++ correctionClassGrade/assets/js/main.js
@@ -1,11 +1,13 @@
 import { Student } from './Student.js';
 
-const app = 
+const app =
 {
     data() {
         return {
             students: [],
-            minGrade: 12
+            minGrade: 12,
+            studentName: '',
+            studentGrade: 0
         }
     },
     async mounted() {
@@ -13,7 +15,7 @@ const app =
         this.students = await response.json();
         console.log(this.students);
 
-        for(let i = 0; i < this.students.length; i++) {
+        for (let i = 0; i < this.students.length; i++) {
             this.students[i] = new Student(this.students[i]);
         }
 
@@ -27,7 +29,7 @@ const app =
         },
         avgGrade() {
             let sum = 0;
-            for(let student of this.students) {
+            for (let student of this.students) {
                 sum += student.grade;
             }
             return (sum / this.nbStudents).toFixed(2);
@@ -35,6 +37,22 @@ const app =
         nbStudentsAboveAvg() {
             let studentsAboveAvg = this.students.filter(student => student.grade > this.avgGrade);
             return studentsAboveAvg.length;
+        }
+
+    },
+    methods: {
+        addStudent() {
+            console.log(this.studentName, this.studentGrade);
+            let tmpStudent = { 'fullname': this.studentName, 'grade': this.studentGrade }
+
+            try {
+                let newStudent = new Student(tmpStudent);
+                this.students.push(newStudent);
+                this.students.sort((a, b) => a.grade - b.grade).reverse();
+            } catch (error) {
+                alert(error.message);
+            }
+
         }
 
     }
