049cc2bf270cf54fce283e12c05d9c7c6bfb0d71
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-11-09 12:38:31 +0100

    061123AM1239

--- cerealTP/assets/js/cerealsBrand.js
+++ cerealTP/assets/js/cerealsBrand.js
@@ -1,9 +1,16 @@
 class cerealsBrand 
 {
     constructor(_cerealsFromJson) {
-        this.iD = '',
-        this.name = '',
-        this.calories = '',
-        
+        this.iD = _cerealsFromJson.id;
+        this.name = '';
+        this.calories = '';
+        this.proteines = '';
+        this.sel = '';
+        this.fibres = '';
+        this.glucides = '';
+        this.sucre ='';
+        this.potassium = '';
+        this.vitamines = '';
+        this.evaluation = '';
     }
 }
